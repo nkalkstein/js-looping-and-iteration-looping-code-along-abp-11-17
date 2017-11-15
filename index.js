4668d32b53cfaa6322d87ec21ee140eaf666bb77
Noah Kalkstein <nkalkstein@gmail.com>
2017-11-15 20:54:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let names = ['Lisa', 'Kaitlin', 'Jan']
 
 function printBadges(names) {
-  let (i=0, i < names.length, i++) {
+  for (let i=0, i < names.length, i++) {
     console.log(`Welcome ${names[i]} You are employee #${i}.`)
   }
 }
